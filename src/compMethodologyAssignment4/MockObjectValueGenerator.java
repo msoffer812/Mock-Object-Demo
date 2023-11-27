6df1d6fbf73783b82adf7fbe0b405d603953c26b
Michelle Soffer <mbrsoffer@gmail.com>
2023-11-27 10:33:36 -0500

    Created mock object class that mimics the genuine class for generating random numbers

---
+++ src/compMethodologyAssignment4/MockObjectValueGenerator.java
@@ -0,0 +1,46 @@
+/**
+ * Mock object that mimics the valid random number for testing
+ */
+package compMethodologyAssignment4;
+
+public class MockObjectValueGenerator implements IRandomValueGenerator{
+	double randomDouble;
+	
+	public MockObjectValueGenerator()
+	{
+		randomDouble = 0;
+	}
+	
+	/**
+	 * 
+	 * set "random" double, used for unit testing
+	 */
+	private void setRandomDouble(double randomDouble)
+	{
+		this.randomDouble = randomDouble;
+	}
+	
+	/**
+	 * @return "random" int between two numbers
+	 * get range,get random amount in the range, then add to min.
+	 */
+	@Override
+	public double getRandomInt(int min, int max)
+	{
+		
+		int newRandomInt = (int)
+				(randomDouble * (max - min)) + min; 
+		
+		return newRandomInt;
+	}
+	
+	/**
+	 * @return boolean based on random probability happening
+	 */
+	@Override
+	public boolean getBooleanProbability(double probability)
+	{
+		return randomDouble < probability;
+	}
+}
+
